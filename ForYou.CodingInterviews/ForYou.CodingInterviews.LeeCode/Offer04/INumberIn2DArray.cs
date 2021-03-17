4cac52bff49fc557a3aabda30d17a00fc7a6005e
muse520@outlook.com <muse520@outlook.com>
2021-03-17 20:51:39 +0800

    feat:NumberIn2DArray

---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer04/INumberIn2DArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer04
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/
+    /// </summary>
+    public interface INumberIn2DArray
+    {
+        bool Exist(int input);
+    }
+}
---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer04/NumberIn2DArrayVer01.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer04
+{
+    public class NumberIn2DArrayVer01 : INumberIn2DArray
+    {
+        private int[][] _matrix;
+
+        public NumberIn2DArrayVer01(int[][] matrix)
+        {
+            _matrix = matrix;
+        }
+
+        public bool Exist(int input)
+        {
+            if (_matrix.Length < 1)
+            {
+                return false;
+            }
+
+            if (_matrix[0].Length < 1)
+            {
+                return false;
+            }
+
+            int height = _matrix.Length;
+            int width = _matrix[0].Length;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                { 
+                    if(_matrix[i][j]> input)
+                    {
+                        break;
+                    }
+                    if (_matrix[i][j] == input)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
