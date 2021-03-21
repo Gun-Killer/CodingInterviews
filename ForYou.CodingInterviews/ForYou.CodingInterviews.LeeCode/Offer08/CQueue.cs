05ab7ba1c73dad8993f6974e71652af7005f95e3
muse520@outlook.com <muse520@outlook.com>
2021-03-21 16:31:02 +0800

    feat:stack to queue

---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer08/CQueue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer08
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+    /// </summary>
+    public class CQueue
+    {
+        Stack<int> _s1 = new Stack<int>();
+        Stack<int> _s2 = new Stack<int>();
+        public CQueue()
+        {
+
+        }
+
+        public void AppendTail(int value)
+        {
+            _s1.Push(value);
+        }
+
+        public int DeleteHead()
+        {
+            if (_s1.Count < 1 )
+            {
+                return -1;
+            }
+
+            while (_s1.Count > 0)
+            {
+                _s2.Push(_s1.Pop());
+            }
+
+            var res = _s2.Pop();
+            while (_s2.Count > 0)
+            {
+                _s1.Push(_s2.Pop());
+            }
+            return res;
+        }
+    }
+}
---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer08/CQueueVer02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer08
+{
+    class CQueueVer02
+    {
+        Stack<int> _s1 = new Stack<int>();
+        Stack<int> _s2 = new Stack<int>();
+        public CQueueVer02()
+        {
+
+        }
+
+        public void AppendTail(int value)
+        {
+            _s1.Push(value);
+        }
+
+        public int DeleteHead()
+        {
+            if (_s2.Count < 1)
+            {
+                if (_s1.Count > 0)
+                {
+                    while (_s1.Count > 0)
+                    {
+                        _s2.Push(_s1.Pop());
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            return _s2.Pop();
+        }
+    }
+}
