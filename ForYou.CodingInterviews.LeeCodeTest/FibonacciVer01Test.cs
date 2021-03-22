23aad090b44ebd1d3eadd1747d7fe977472f0ba8
muse520@outlook.com <muse520@outlook.com>
2021-03-22 19:23:33 +0800

    feat:Fibonacci

---
+++ ForYou.CodingInterviews.LeeCodeTest/FibonacciVer01Test.cs
@@ -0,0 +1,26 @@
+﻿using ForYou.CodingInterviews.LeeCode.Offer09;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCodeTest
+{
+    public class FibonacciVer01Test
+    {
+        FibonacciVer01 fibonacci; 
+        [SetUp]
+        public void Init()
+        {
+            fibonacci = new FibonacciVer01();
+        }
+        [TestCase(2,1)]
+        [TestCase(5,5)]
+        public void FibTest(int input, int result)
+        {
+            Assert.That(fibonacci.Fib(input) == result);
+        }
+    }
+}
---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer09/FibonacciVer01.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer09
+{
+    public class FibonacciVer01
+    { 
+        public int Fib(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+            int first = 0;
+            int second = 1;
+            for (int i = 1; i < n; i++)
+            {
+                second = first + second;
+                first = second - first;
+                second = second % 1000000007;
+            }
+            return second;
+        }
+    }
+}
