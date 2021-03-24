603c76934133eb25469097a55e8deb9772c7867e
muse520@outlook.com <muse520@outlook.com>
2021-03-24 21:12:57 +0800

    feat:NumWays

---
+++ ForYou.CodingInterviews.LeeCodeTest/NumWaysVer01Test.cs
@@ -0,0 +1,29 @@
+﻿using ForYou.CodingInterviews.LeeCode.Offer10;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCodeTest
+{
+    public class NumWaysVer01Test
+    {
+        NumWaysVer01 _svc;
+        [SetUp]
+        public void Init()
+        {
+            _svc = new NumWaysVer01();
+        }
+        [TestCase(1,1)]
+        [TestCase(2,2)]
+        [TestCase(3,3)]
+        [TestCase(7,21)]
+        public void NumWaysTest(int input,int result)
+        {
+            var assert=_svc.NumWays(input) == result;
+            Assert.IsTrue(assert);
+        }
+    }
+}
---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer10/NumWaysVer01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer10
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
+    /// </summary>
+    public class NumWaysVer01
+    {
+        public int NumWays(int n)
+        {
+            if (n < 2)
+            {
+                return 1;
+            }
+
+            int first = 1;
+            int second = 1;
+            for (int i = 1; i < n; i++)
+            {
+                second = first + second;
+                first = second - first;
+                second = second % 1000000007;
+            }
+            return second;
+        }
+    }
+}
