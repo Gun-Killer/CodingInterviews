abebc195fb714b6292650c6b14a94f54c6c66d8f
muse520@outlook.com <muse520@outlook.com>
2021-03-27 21:25:05 +0800

    feat:保存解题思路

---
+++ ForYou.CodingInterviews.LeeCodeTest/PathExistVer01Test.cs
@@ -0,0 +1,47 @@
+﻿using ForYou.CodingInterviews.LeeCode.Offer12;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCodeTest
+{
+    public class PathExistVer01Test
+    {
+        PathExistVer01 _existVer01;
+
+        [SetUp]
+        public void Init()
+        {
+            _existVer01 = new PathExistVer01();
+        }
+
+
+        [Test]
+        public void Exist()
+        {
+            var input = new char[][]
+            {
+                new char []{ 'a','b','c','e'},
+                new char [] { 's','f','c','s'},
+                new char [] { 'a','d','e','e'},
+            };
+            Assert.That(_existVer01.Exist(input, "bfce"));
+            input = new char[][]
+             {
+                    new char []{ 'a','b'},
+                    new char [] { 'c','d'}
+             };
+            Assert.That(_existVer01.Exist(input, "acdb"));
+
+            input = new char[][]
+            {
+                    new char []{ 'a'}
+            };
+            Assert.That(_existVer01.Exist(input, "ab") == false);
+
+        }
+    }
+}
---
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer12/PathExistVer01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingInterviews.LeeCode.Offer12
+{
+    /// <summary>
+    /// https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
+    /// 
+    /// 解题思路，先找到第一个匹配的字符位置，以该位置上下左右在进行寻找下个字符位置，一直重复。找到了继续
+    /// 找不到重新在开始匹配。重复上面过程
+    /// </summary>
+    public class PathExistVer01
+    {
+        public bool Exist(char[][] board, string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+            if ((board[0].Length * board.Length) < word.Length)
+            {
+                return false;
+            }
+            var wordSpan = word.AsSpan();
+            var endIndex = word.Length;
+
+
+           
+            return false;
+        }
+    }
+}
