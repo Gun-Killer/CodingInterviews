37f780e0b79fd458d6839fbce7793e36f45fc47d
muse520@outlook.com <muse520@outlook.com>
2021-03-29 19:37:35 +0800

    feat:第一次提交

--- ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer12/PathExistVer01.cs
+++ ForYou.CodingInterviews/ForYou.CodingInterviews.LeeCode/Offer12/PathExistVer01.cs
@@ -27,9 +27,45 @@ namespace ForYou.CodingInterviews.LeeCode.Offer12
             var wordSpan = word.AsSpan();
             var endIndex = word.Length;
 
+            for (int i = 0; i < board.Length; i++)
+            {
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    if (Find(i, j, board, 0, wordSpan))
+                    {
+                        return true;
+                    }
+                }
+            }
 
-           
             return false;
+        }
+
+        private bool Find(int row, int column, char[][] board, int index, ReadOnlySpan<char> word)
+        {
+            if (row < 0 || column < 0 || row >= board.Length || column >= board[0].Length)
+            {
+                return false;
+            }
+
+            if (board[row][column] != word[index])
+            {
+                return false;
+            }
+
+            if (index == (word.Length - 1))
+            {
+                return true;
+            }
+            index++;
+            var mid=board[row][column];
+            board[row][column] = '~';
+            var res= Find(row - 1, column, board, index, word)
+                || Find(row, column - 1, board, index, word)
+                || Find(row + 1, column, board, index, word)
+                || Find(row, column +1, board, index, word);
+            board[row][column] = mid;
+            return res;
         }
     }
 }
