5d7b64c26b967a5f285704fbe9701394efa93c90
muse520@outlook.com <muse520@outlook.com>
2021-04-11 16:06:58 +0800

    feat:binding

---
+++ ForYou.CodingInterviews/WPF/深入浅出WPF/ForYou.CodingIntereviews.WPFToDo/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingIntereviews.WPFToDo.Models
+{
+    public class MenuItem
+    {
+        public string Icon { get; set; }
+
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ ForYou.CodingInterviews/WPF/深入浅出WPF/ForYou.CodingIntereviews.WPFToDo/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ForYou.CodingIntereviews.WPFToDo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForYou.CodingIntereviews.WPFToDo.ViewModels
+{
+    public class MenuItemViewModel
+    {
+
+        public MenuItemViewModel()
+        {
+            Items = new ObservableCollection<MenuItem>()
+            {
+                new MenuItem()
+                {
+                    Icon="图标",
+                    Name ="我的一天",
+                    Count=10
+                },
+                 new MenuItem()
+                {
+                    Icon="图标",
+                    Name ="我的一天",
+                    Count=10
+                }
+            };
+        }
+        public ObservableCollection<MenuItem> Items { get; set; }
+    }
+}
