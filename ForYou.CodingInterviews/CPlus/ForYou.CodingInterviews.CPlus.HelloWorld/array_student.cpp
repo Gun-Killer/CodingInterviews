87308b1791be393348b5fe834c703f88916078c8
muse520@outlook.com <muse520@outlook.com>
2021-04-18 21:31:05 +0800

    feat:暂存

--- ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlus.HelloWorld/array_student.cpp
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlus.HelloWorld/array_student.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+int main5()
 {
 	int nums[5];
 
---
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlus.HelloWorld/method_student.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int add(int num1, int num2)
+{
+
+	return num1 + num2;
+
+}
+
+int main()
+{
+	cout << add(1, 2) << endl;
+	return 0;
+}
+
