23ac85d60c5b185a348c0b61ff62819f9dd27fba
muse520@outlook.com <muse520@outlook.com>
2021-04-24 22:24:07 +0800

    feat:暂存

--- ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.ObjectLearn/program.cpp
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.ObjectLearn/program.cpp
@@ -13,5 +13,18 @@ int main()
 
 	delete int_num_1;
 	delete[] arr;
+
+
+	int num2 = 10;
+	int* p = &num2;
+	cout << num2 << endl;
+
+	*p = 20;
+	cout << num2 << endl;
+
+	int& num3 = num2;//给变量num2起个别名num3
+	num3 = 30;
+	cout << num2 << endl;
+
 	return 0;
 }
