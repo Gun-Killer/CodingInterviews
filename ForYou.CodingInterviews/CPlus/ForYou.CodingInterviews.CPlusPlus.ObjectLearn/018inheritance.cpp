0c11e92f74be6fe803ddb2e40c819e976ffcf9c4
muse520@outlook.com <muse520@outlook.com>
2021-05-16 15:04:09 +0800

    feat:c++ inheritance

---
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.ObjectLearn/018inheritance.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+namespace CPP
+{
+	class PersonObject
+	{
+	public:
+		PersonObject();
+		~PersonObject();
+
+		string m_name;
+		int m_age;
+	private:
+
+	};
+
+	PersonObject::PersonObject()
+	{
+	}
+
+	PersonObject::~PersonObject()
+	{
+	}
+
+	class  ManObecjt:public PersonObject
+	{
+	public:
+		ManObecjt();
+		~ManObecjt();
+
+	private:
+
+	};
+
+	ManObecjt::ManObecjt()
+	{
+		
+	}
+
+	ManObecjt::~ManObecjt()
+	{
+	}
+}
+
+int main()
+{
+	CPP::ManObecjt man_obj1;
+	man_obj1.m_name = "name";
+	man_obj1.m_age = 20;
+
+	cout << man_obj1.m_name << endl;
+	return 0;
+}
