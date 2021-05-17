6650f73ea78446bb754c94333f2e3daa670f0328
muse520@outlook.com <muse520@outlook.com>
2021-05-17 22:06:00 +0800

    using: c++ write file

---
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.File/001File.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	ofstream outfile;
+	outfile.open("123.txt", ios::out);
+	outfile.write("121", 3);
+	outfile << 10000;
+	outfile << 10001 << endl;
+	outfile << 10002;
+	outfile.close();
+	return 0;
+}
