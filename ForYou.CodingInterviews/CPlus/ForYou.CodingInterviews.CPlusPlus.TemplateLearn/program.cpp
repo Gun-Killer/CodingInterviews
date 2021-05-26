d32ff174f7dbbeeb5303e2b53aba3c3281a9fa5b
muse520@outlook.com <muse520@outlook.com>
2021-05-26 22:11:10 +0800

    feat: c++ template

---
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.TemplateLearn/program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+template <typename T>
+void swap(T& input1, T& input2)
+{
+    T temp = input1;
+    input1 = input2;
+    input2 = temp;
+}
+
+int main()
+{
+    int num1 = 24;
+    int num2 = 34;
+
+    ::swap(num1, num2);
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    return 0;
+}
