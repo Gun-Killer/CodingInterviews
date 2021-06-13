0e795883ba043d9318c0e54ebd9d9a68a5574b44
muse520@outlook.com <muse520@outlook.com>
2021-06-13 11:08:15 +0800

    feat:c++

--- ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.TemplateLearn/015.cpp
+++ ForYou.CodingInterviews/CPlus/ForYou.CodingInterviews.CPlusPlus.TemplateLearn/015.cpp
@@ -5,16 +5,32 @@ using namespace std;
 
 class MethodObject
 {
+private:
+    int m_count = 0;
 public:
     int operator()(int num1, int num2)
     {
+        m_count++;
         return num1 + num2;
+    }
+
+    int executeNum()
+    {
+        return m_count;
     }
 };
 
 int main()
 {
     MethodObject obj;
-    cout << obj(1, 2) << endl;
+    srand(time(NULL));
+    for (int i = 0; i < 5; i++)
+    {
+        cout << obj(rand(), rand()) << endl;
+    }
+
+    cout << "************" << endl;
+
+    cout << obj.executeNum() << endl;
     return 0;
 }
