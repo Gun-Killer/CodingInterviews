5ee58f3f4d40367612acdab9e7f9103f047f6607
muse520@outlook.com <muse520@outlook.com>
2021-06-21 22:17:37 +0800

    c++: 进制

--- ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus002/003.cpp
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus002/003.cpp
@@ -11,4 +11,9 @@ int main()
     cout << "十进制" << num1 << endl;
     cout << "十六进制" << num2 << endl;
     cout << "8进制" << num3 << endl;
+
+    cout << "-----------------" << endl;
+    cout << "十进制" << num1 << endl;
+    cout << "十六进制" << hex << num2 << endl;
+    cout << "8进制" << oct << num3 << endl;
 }
