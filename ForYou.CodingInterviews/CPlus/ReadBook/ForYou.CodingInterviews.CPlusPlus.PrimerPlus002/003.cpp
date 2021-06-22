dae7c7a2ac341666435942b494d4bf8f918b0c98
muse520@outlook.com <muse520@outlook.com>
2021-06-22 21:46:16 +0800

    feat:

--- ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus002/003.cpp
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus002/003.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int main()
+int main003()
 {
     int num1 = 42;
     int num2 = 0x42;
@@ -16,4 +16,5 @@ int main()
     cout << "十进制" << num1 << endl;
     cout << "十六进制" << hex << num2 << endl;
     cout << "8进制" << oct << num3 << endl;
+    return 0;
 }
---
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus002/004.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float num1 = 10.6e20f;
+
+    cout << num1 << endl;
+
+    cout << 8.0f << endl;
+    cout << 8.0e+5 << endl;
+    cout << 8.0e+5 << endl;
+
+
+    cout << "-------------------" << endl;
+
+    cout << sizeof(float) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(long double) << endl;
+
+    cout << "-------------------" << endl;
+
+    float num2 = 10.0f / 3.0f;
+    double num3 = 10.0 / 3.0;
+
+    const float million = 1.0e6f;
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    cout << num2 * million << endl;
+    cout << num3 * million << endl;
+
+    cout << num2 * million * 10.0f << endl;
+    cout << num3 * million *10.0f << endl;
+
+    return 0;
+}
