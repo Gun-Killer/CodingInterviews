cee8112310af4feda6bf9a98347fb231151e9a2f
muse520@outlook.com <muse520@outlook.com>
2021-07-09 16:20:14 +0800

    feat:c++

--- ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus004/012.cpp
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus004/012.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include <array>
 
 using namespace std;
 
 void learn04_01();
 void learn04_02();
 void learn04_03();
+void learn04_04();
+void learn04_07();
+void learn04_08();
+void learn04_10();
 
 struct CandyBar;
-
+struct Pizza;
 
 int main()
 {
@@ -16,7 +21,15 @@ int main()
 
     //learn04_02();
 
-    learn04_03();
+    //learn04_03();
+
+    //learn04_04();
+
+    //learn04_07();
+
+    //learn04_08();
+
+    learn04_10();
     return 0;
 }
 
@@ -88,4 +101,105 @@ void learn04_03()
     };
 
 
+}
+
+void learn04_04()
+{
+    cout << "------------------------4------------------------" << endl;
+    CandyBar cb[3]{
+        {
+            "Mocha Munch",
+            2.3f,
+            350
+        },
+        {
+            "Mocha Munch",
+            2.3f,
+            350
+        },
+        {
+            "Mocha Munch",
+            2.3f,
+            350
+        }
+    };
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "mrand: " << cb[i].m_mrand
+            << "weight: " << cb[i].m_weight
+            << "calories: " << cb[i].m_calories
+            << endl;
+    }
+}
+struct Pizza
+{
+    string m_company_name;
+    int m_diameter;
+    float m_weight;
+};
+void learn04_07()
+{
+    cout << "------------------------7------------------------" << endl;
+    Pizza p;
+    cout << "Enter company name: ";
+    cin >> p.m_company_name;
+    cout << endl;
+
+    cout << "Enter diameter: ";
+    cin >> p.m_diameter;
+    cout << endl;
+
+    cout << "Enter weight: ";
+    cin >> p.m_weight;
+    cout << endl;
+    cout << "Pizza company name is " << p.m_company_name
+        << " diameter is " << p.m_diameter
+        << " weight is " << p.m_weight
+        << endl;
+}
+
+void learn04_08()
+{
+    cout << "------------------------8------------------------" << endl;
+    Pizza* p = new Pizza();
+
+    cout << "Enter diameter: ";
+    cin >> p->m_diameter;
+    cout << endl;
+
+    cout << "Enter company name: ";
+    cin >> p->m_company_name;
+    cout << endl;
+
+
+
+    cout << "Enter weight: ";
+    cin >> p->m_weight;
+    cout << endl;
+    cout << "Pizza company name is " << p->m_company_name
+        << " diameter is " << p->m_diameter
+        << " weight is " << p->m_weight
+        << endl;
+    delete p;
+}
+
+void learn04_10()
+{
+    cout << "------------------------10------------------------" << endl;
+    array<float, 3> p;
+    for (int i = 0; i < p.size(); i++)
+    {
+        cout << "Enter " << i << " 40M score: ";
+        cin >> p[i];
+    }
+
+    float sum = 0.0f;
+    for (int i = 0; i < p.size(); i++)
+    {
+        sum += p[i];
+    }
+
+    cout << "total score is " << sum
+        << ", average score is " << sum / p.size()
+        << endl;
 }
