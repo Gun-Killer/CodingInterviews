6a5b32e9cbc196ddeffb8d743e7d98572c237467
muse520@outlook.com <muse520@outlook.com>
2021-08-07 23:06:19 +0800

    feat:c++

---
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus012/MyQueue.h
@@ -0,0 +1,98 @@
+#pragma once
+#include <exception>
+#include <stdexcept>
+
+template<typename NoteTypeT>
+class MyQueue
+{
+    struct QueueNode
+    {
+        NoteTypeT item;
+        QueueNode* next = nullptr;
+    };
+
+private:
+    QueueNode* first = nullptr;
+    QueueNode* end = nullptr;
+    int m_size{ 0 };
+
+public:
+    MyQueue();
+    MyQueue(MyQueue& source);
+
+    ~MyQueue();
+
+    int size();
+
+    void enqueue(const NoteTypeT& item);
+
+    NoteTypeT& dequeue();
+};
+
+template<typename NoteTypeT>
+inline MyQueue<NoteTypeT>::MyQueue()
+{
+
+}
+
+template<typename NoteTypeT>
+inline MyQueue<NoteTypeT>::MyQueue(MyQueue& source)
+{
+    auto temp = source.first;
+    while (temp != nullptr)
+    {
+        enqueue(temp->item);
+        temp = temp->next;
+    }
+}
+
+template<typename NoteTypeT>
+inline MyQueue<NoteTypeT>::~MyQueue()
+{
+    while (this->first != nullptr)
+    {
+        auto temp = this->first->next;
+        delete this->first;
+        this->first = temp;
+    }
+    this->end = nullptr;
+}
+
+template<typename NoteTypeT>
+inline void MyQueue<NoteTypeT>::enqueue(const NoteTypeT& item)
+{
+    m_size++;
+    if (first == nullptr)
+    {
+        first = new QueueNode{ item , nullptr };
+        end = first;
+    }
+    else
+    {
+        auto temp = new QueueNode{ item , nullptr };
+        end->next = temp;
+        end = temp;
+    }
+}
+
+
+template<typename NoteTypeT>
+int MyQueue<NoteTypeT>::size()
+{
+    return this->m_size;
+}
+
+template<typename NoteTypeT>
+NoteTypeT& MyQueue<NoteTypeT>::dequeue()
+{
+    if (m_size < 1)
+    {
+        throw std::out_of_range::out_of_range("");
+    }
+    m_size--;
+    auto temp = first;
+    first = first->next;
+    NoteTypeT res = temp->item;
+    delete temp;
+    return res;
+}
--- ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus012/main.cpp
+++ ForYou.CodingInterviews/CPlus/ReadBook/ForYou.CodingInterviews.CPlusPlus.PrimerPlus012/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+#include "MyQueue.h"
+
+using namespace std;
+
 int main()
 {
+    /* MyQueue<int> q;
+
+     q.enqueue(1);
+     q.enqueue(2);
+     q.enqueue(3);
+
+     while (q.size() > 0)
+     {
+         cout << q.dequeue() << "\t";
+     }*/
+
+    int num1 = 1;
+    int& num2 = num1;
+    int num3 = num2;
+
+    cout << &num3 << "\t" << &num1 << "\t" << &num2;
     return 0;
 }
