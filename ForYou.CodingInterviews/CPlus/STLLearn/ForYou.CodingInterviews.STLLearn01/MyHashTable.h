bb7e0f24bed6479d8be9d15999ca54630d819323
muse520@outlook.com <muse520@outlook.com>
2021-09-09 22:19:44 +0800

    feat:add

--- ForYou.CodingInterviews/CPlus/STLLearn/ForYou.CodingInterviews.STLLearn01/MyHashTable.h
+++ ForYou.CodingInterviews/CPlus/STLLearn/ForYou.CodingInterviews.STLLearn01/MyHashTable.h
@@ -3,19 +3,23 @@ namespace Killer
 {
     template<typename TKey, typename TValue, class THashFunc, typename TEqualKey>
     class MyHashTable
-    {    
+    {
+
     private:
         class BucketNode
         {
             const TKey key;
-            int value_index{0};
+            int value_index{ -1 };
             BucketNode* next;
         };
         BucketNode* buckets{ nullptr };
         int size{ 0 };
-        int capacity{0}
+        int capacity{ 0 };
         TValue* real_value{ nullptr };
         THashFunc hash_func;
+
+        using node_type = typename BucketNode;
+
     public:
         MyHashTable();
 
@@ -29,15 +33,24 @@ namespace Killer
     {
         capacity = 10;
         real_value = new TValue[capacity];
-        buckets = new BucketNode[capacity];
+        buckets = new node_type[capacity];
     }
     template<typename TKey, typename TValue, class THashFunc, typename TEqualKey>
     inline void MyHashTable<TKey, TValue, THashFunc, TEqualKey>::Add(const TKey& key_arg, const TValue& value_arg)
     {
-       int hash = hash_func(key_arg);
-       var index = hash % capacity;
-        
-
+        int hash = hash_func(key_arg);
+        int index = hash % capacity;
+        node_type& temp = buckets[index];
+        if (temp.value_index == -1)
+        {
+            buckets[index] = new node_type{ key_arg, size, nullptr };
+        }
+        else
+        {
+            temp.next = new node_type{ key_arg, size, nullptr };
+        }
+        real_value[size] = value_arg;
+        size++;
     }
 
     template<typename TKey, typename TValue, class THashFunc, typename TEqualKey>
@@ -46,6 +59,16 @@ namespace Killer
         if (buckets == nullptr)
         {
             return;
+        }
+        for (auto item : buckets)
+        {
+            auto next = item.next;
+            while (next != nullptr)
+            {
+                auto temp = next.next;
+                delete next;
+                next = temp;
+            }
         }
         delete[] buckets;
         delete[] real_value;
