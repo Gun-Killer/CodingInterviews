0fc4475f4a4e8f4eba764c6a6b4176e8bb3340d6
muse520@outlook.com <muse520@outlook.com>
2021-09-26 22:04:25 +0800

    feat:initializer_list

--- ForYou.CodingInterviews/CPlus/STLLearn/ForYou.CodingInterviews.STLLearn02/main.cpp
+++ ForYou.CodingInterviews/CPlus/STLLearn/ForYou.CodingInterviews.STLLearn02/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stddef.h>
 #include <functional>
+#include <initializer_list>
 
 using namespace std;
 
@@ -17,12 +18,22 @@ void print(const T& first, const TArgs&... args)
     print(args...);
 }
 
+void init_list_test(initializer_list<int> list)
+{
+    for (int item : list)
+    {
+        cout << item << endl;
+    }
+}
+
 int main()
 {
 
     std::cout << __cplusplus << std::endl;
 
     print(1, 3.5, "123", max(1, 3), 20);
+
+    init_list_test({ 1, 2, 3 });
 
     return 0;
 }
